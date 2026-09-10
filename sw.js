47eafd69541299605afa644e215ca2a527dd92e9
samiranaa196-cell <samiranaa196@gmail.com>
2026-09-10 17:57:14 +0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,83 @@
+const CACHE_NAME = "x-calculator-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icon.svg"
+];
+
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+
+    );
+
+    self.skipWaiting();
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(cachedResponse => {
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+                    .then(response => {
+
+                        const copy = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(
+                                    event.request,
+                                    copy
+                                );
+                            });
+
+                        return response;
+
+                    })
+                    .catch(() =>
+                        caches.match("./index.html")
+                    );
+
+            })
+
+    );
+
+});
